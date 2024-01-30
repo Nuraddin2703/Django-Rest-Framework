7bf599b09e336250c86cffe0b48ea2a36430dcf0
Nuraddin Mamedov <mamedov003.bmstu@yandex.ru>
2024-01-30 18:28:00 +0300

    Испрааил отображение в панели администратора

--- djangoTest/teammate/models.py
+++ djangoTest/teammate/models.py
@@ -6,6 +6,9 @@ class Teammate(models.Model):
     lastname = models.CharField(max_length=255)
     key = models.ForeignKey('Team', on_delete=models.PROTECT, null=True)
 
+    def __str__(self):
+        return self.lastname
+
     class Meta:
         verbose_name = 'Состав команды'
         verbose_name_plural = 'Состав команды'
@@ -13,6 +16,9 @@ class Teammate(models.Model):
 
 class Team(models.Model):
     team_name = models.CharField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return self.team_name
 
     class Meta:
         verbose_name = 'Команды'
