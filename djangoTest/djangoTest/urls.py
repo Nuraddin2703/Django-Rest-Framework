44cbc7175a2d768d957101de112f822cff5d3971
Nuraddin Mamedov <mamedov003.bmstu@yandex.ru>
2024-02-20 12:24:52 +0300

    Добавил лишние файлы в gitignore, написал документацию

--- djangoTest/djangoTest/urls.py
+++ djangoTest/djangoTest/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import TeammateAPIView, QuoteAPIView
+from teammate.views import TeammateAPIView, QuoteAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
